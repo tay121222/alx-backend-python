3f9e926283b64b87770e59efeeff7f2611477901
Enoch Taylor <tay121222@gmail.com>
2024-01-14 22:03:57 +0000

    More involved type annotations - Using Optional

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Type annotated function"""
-from typing import Mapping, TypeVar, Union, Any
+from typing import Mapping, TypeVar, Union, Any, Optional
 
 T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
+                     default: Optional[T] = None) -> Union[Any, T]:
     """Type annotated function safely_get_value"""
     if key in dct:
         return dct[key]
