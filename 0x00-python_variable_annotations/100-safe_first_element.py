c4ca5034226c2403e5688181470e6ee14285ead8
Enoch Taylor <tay121222@gmail.com>
2024-01-14 21:39:21 +0000

    Duck typing - first element of a sequence - fixed pycodestyle

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,6 +2,7 @@
 """duck-typed annotations"""
 from typing import Sequence, Any, Union
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Ducked-typed annotation function that takes a sequence (1st) and
     returns the first element if the sequence is not empty"""
