b457a2bac8c23bcb5cbd00cdb7b25ca194ffbd52
Enoch Taylor <tay121222@gmail.com>
2024-01-14 13:46:44 +0000

    function element_length that takes an iterable of sequences

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """function element_length that takes an iterable of sequences"""
     return [(i, len(i)) for i in lst]
