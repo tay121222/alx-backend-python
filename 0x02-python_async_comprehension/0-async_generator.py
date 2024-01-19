4c6e75aef40bb1d4e4f26d0ca54da5f17d090bed
Enoch Taylor <tay121222@gmail.com>
2024-01-19 23:59:53 +0000

    coroutine called async_generator that takes no arguments - added hint for return

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,6 +2,7 @@
 """coroutine called async_generator that takes no arguments"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
