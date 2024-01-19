dbfc1a36e63397dab9baf286028212be9bafce7e
Enoch Taylor <tay121222@gmail.com>
2024-01-19 23:58:10 +0000

    coroutine called async_generator that takes no arguments - added hint for return

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """coroutine will loop 10 times, each time asynchronously wait
     1 second, then yield a random number between 0 and 10"""
     i = 0
