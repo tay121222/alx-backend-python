4420eae7ceee11e696e0e68d41381da5169c51ed
Enoch Taylor <tay121222@gmail.com>
2024-01-20 00:08:06 +0000

    coroutine called async_generator that takes no arguments - changed asyncgenerator to generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """coroutine called async_generator that takes no arguments"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """coroutine will loop 10 times, each time asynchronously wait
     1 second, then yield a random number between 0 and 10"""
     i = 0
