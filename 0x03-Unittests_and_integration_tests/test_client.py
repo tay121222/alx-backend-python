3b01f1e4a7f410ffca13d8b52402e23abf78a7ce
Enoch Taylor <tay121222@gmail.com>
2024-02-11 20:52:45 +0000

    test that GithubOrgClient.org returns the correct value

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""contains client.GithubOrgClient class - UnitTest"""
+import unittest
+from unittest.mock import patch
+from client import GithubOrgClient
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test Github Org Client"""
+    @parameterized.expand([
+        ('google',),
+        ('abc',)
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock):
+        """test that GithubOrgClient.org returns the correct value"""
+        org_client = GithubOrgClient(org_name)
+        result = org_client.org
+        url = GithubOrgClient.ORG_URL.format(org=org_name)
+        mock_get_json.assert_called_once_with(url)
