3053835e9b77ac673d25558e2b848d425ca44890
Enoch Taylor <tay121222@gmail.com>
2024-02-11 23:00:48 +0000

    6. More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,10 +20,6 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(results, mock.return_value)
         mock.assert_called_once
 
-
-class TestGithubOrgClient(unittest.TestCase):
-    """Test Github Org Client"""
-
     def test_public_repos_url(self):
         """Test _public_repos_url property"""
 
@@ -37,3 +33,23 @@ class TestGithubOrgClient(unittest.TestCase):
             org_client = GithubOrgClient("test_org")
             public_repos_url = org_client._public_repos_url
             self.assertEqual(public_repos_url, known_payload["repos_url"])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        """Test public_repos method"""
+
+        payload = [
+            {"name": "Django", },
+            {"name": "Luxand", },
+        ]
+        uurl = "https://api.github.com/orgs/test_org/repos"
+        mock_public_repos_url.return_value = uurl
+        mock_get_json.return_value = payload
+        org_client = GithubOrgClient("test_org")
+        repos = org_client.public_repos()
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once_with(
+                "https://api.github.com/orgs/test_org/repos"
+                )
+        expected_repos = ["Django", "Luxand",]
+        self.assertEqual(repos, expected_repos)
